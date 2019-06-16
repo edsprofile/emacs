b22c1325bca39e28562b904d1643f07145da28c4
Edwin <31055689+edsprofile@users.noreply.github.com>
2019-06-16 09:56:33 -0400

    Add files via upload

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Edwin's init.el
 ;; start date: 6/08/2019
-;; last modified: 6/09/2019
+;; last modified: 6/16/2019
 ;; My personal init.el to learn more about emacs.
 ;; --------------------------------------------------------------------------------
 ;; My approach to writting this file: any new features should be under the super key
@@ -14,27 +14,29 @@
 ;; --------------------------------------------------------------------------------
 ;; Recent changes:
 ;; - Change: ibuffer from opening in other window to open in current window.
-
-;; - Reason: The reason for this was it just feels more like what emacs would logically do.
+;; - Change: removed using ansi-term and other stuff I want to make thing simpler again.
+;; - Change: Removed all code that was commented out.
 
 ;; Remove the start screen and basic emacs settings
 (setq inhibit-startup-message t)
+;; Instead of an annoying sound a visual action is better I feel
 (setq visible-bell t)
+;; Remove tool bar less clutter
 (tool-bar-mode -1)
+;; Remove menu bar
 (menu-bar-mode -1)
+;; Remove the scroll bar
 (scroll-bar-mode -1)
+;; Add line number and column numbers to be displayed on emacs bar
 (line-number-mode 1)
 (column-number-mode 1)
+;; Add line numbers to be displayed on the left side
 (global-display-line-numbers-mode 1)
-;; Debating if I should add time I think I would need a better for it but I like the current bar.
-;; (setq display-time-24hr-format t)
-;; (setq display-time-format "%H:%M - %d %B %Y")
-;; (setq display-time-mode 1)
-;; Allow buffer listing and matching in the minibuffer when using e.g. C-x b
-(setq indo-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-;; Buffer stuff
+;; display time standard AM/PM
+(display-time-mode 1)
+
+;; ***** BUFFER *****
+
 ;; Setting C-x k to kill the current buffer when pressed
 (defun kill-current-buffer ()
   "Sets C-x k to kill the current buffer."
@@ -45,7 +47,7 @@
 ;; Setting ibuffer to be the default buffer and opens in other window
 (defalias 'list-buffers 'ibuffer)
 
-;; ***** CUSTOM FUNCTION *****
+;; ***** CUSTOM FUNCTIONS *****
 
 ;; When opening a new window the cursor will be active in that new window
 ;; these are custom function for following the cursor.
@@ -79,7 +81,14 @@
 
 ;; ***** PACKAGES BELOW *****
 
+;; Adding a powerline for better emacs bar
+(use-package powerline
+  :ensure t)
+(powerline-default-theme)
+
 ;; Adding use-ttf to keep font consistent on different machines
+;; Currently I have a need for this but may get removed once
+;; I do not have to switch machinces so often.
 (use-package use-ttf
   :ensure t)
 (setq use-tff-default-ttf-font-name "Ubuntu mono")
@@ -88,30 +97,19 @@
 (use-package sudo-edit
   :ensure t)
 
-;; Adding vertical ido just easier to look at a vertical list than a horizontal one
-(use-package ido-vertical-mode
-  :ensure t)
-(ido-vertical-mode 1)
-
-;; Adding dmenu to easier see launchable applications in Exwm
+;; Adding dmenu to easier see launchable applications
 (use-package dmenu
   :ensure t
   :bind
     ("s-d" . 'dmenu))
-
-;; Adding a shell emulator this really isn't a package
-;; so I will probably move it somewhere else in the furture.
-(defvar bash-shell "/bin/bash")
-(defadvice ansi-term (before force-bash)
-  (interactive (list bash-shell)))
-(ad-activate 'ansi-term)
-(global-set-key (kbd "s-e") 'ansi-term)
 
 ;; Adding popup-kill-ring to show the kill ring
 (use-package popup-kill-ring
   :ensure t
   :bind
   ("M-y" . popup-kill-ring))
+
+;; ***** EXWM *****
 
 ;; Adding EXWM an emacs windows manager for those long work days
 ;; for this to work you need the proper file e.g. /usr/share/xsessions/emacs.desktop
@@ -120,34 +118,11 @@
   :ensure t
   :config
   (require 'exwm-config)
-  (exwm-config-default)
-  (exwm-config-ido)
   (dotimes (i 10)
       (exwm-input-set-key (kbd (format "s-%d" i))
                           `(lambda ()
                              (interactive)
-                             (exwm-workspace-switch-create ,i))))
-  ;; I tried to set up the media key for this but could not figure it out
-  ;; if anyone figues out let me know I also tried another package called desktop enviroment
-  ;; it worked better but again not fully.
-  (dolist (k '(XF86AudioLowerVolume
-               XF86AudioRaiseVolume
-               XF86PowerOff
-               XF86AudioMute
-               XF86AudioPlay
-               XF86AudioStop
-               XF86AudioPrev
-               XF86AudioNext
-               XF86ScreenSaver
-               XF68Back
-               XF86Forward
-               Scroll_Lock
-               print))
-    (cl-pushnew k exwm-input-prefix-keys))
-  (exwm-input-set-key (kbd "s-i")
-                        (lambda (command)
-                          (interactive (list (read-shell-command "$ ")))
-                          (start-process-shell-command command nil command))))
+                             (exwm-workspace-switch-create ,i)))))
 ;; This enables the system tray for EXWM so is in included here
 (require 'exwm-systemtray)
 (exwm-systemtray-enable)
@@ -175,7 +150,7 @@
   (dashboard-setup-startup-hook)
   (setq dashboard-items '((recents . 5)))
   (setq dashboard-banner-logo-title "Welcome to Emacs")
-  (setq dashboard-startup-banner 'official)
+  (setq dashboard-startup-banner 'logo)
   (setq dashboard-set-footer nil))
 
 ;; Adding auto-complete it will search through all buffer
@@ -193,17 +168,27 @@
   :bind
   ("M-s" . avy-goto-char))
 
-;; Adding ivy, swiper and counsel
-;; Adding counsel will bring in ivy and swiper
-;; Swiper allows for searching through the file simliar to isearch but better
-;; Counsel allows for searching through M-x functions better by bringing up a menu and matching
-;; Counsel also does the same for files.
-(use-package counsel
+;; Adding swiper
+(use-package swiper
+  :ensure t
+  :bind ("C-s" . 'swiper))
+
+;; trying out helm for a little while to see what I think
+(use-package helm
   :ensure t
   :config
-  (global-set-key (kbd "C-s") 'swiper)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file))
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (setq helm-autoresize-max-height 0
+	helm-autoresize-min-height 40
+	helm-M-x-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-recentq-fuzzy-match t
+	helm-semantic-fuzzy-match t
+	helm-imenu-fuzzy-match t))
+(helm-mode 1)
+(helm-autoresize-mode 1)
 
 ;; Adding switch-window
 (use-package switch-window
@@ -226,10 +211,12 @@
   :config (which-key-mode))
 
 ;; Org mode Bullets changed the astrisks to fonts
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
+;; kind of distracting so I removed it for now but it is nice
+;; so I am debating with keeping it or not.
+;; (use-package org-bullets
+;;   :ensure t
+;;   :config
+;;   (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
 
 ;; Adding Beacon which just shows cursor a little more disticly when switching windows
 (use-package beacon
@@ -237,22 +224,15 @@
   :config
   (beacon-mode 1))
 
+;; linux kernel C programming settings
+(setq c-default-style "linux"
+      c-basic-offset 8)
+
 ;;|------------------------------------------------------------------------------|
 ;;|                                                                              |
 ;;|                          Extra added variables                               |
 ;;|                                                                              |
 ;;|------------------------------------------------------------------------------|
-
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "DAMA" :family "Ubuntu mono")))))
-
-
-;; Custom variables
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -260,4 +240,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (better-shell use-ttf desktop-environment desktop-enviroment fancy-battery ido-vertical-mode dmenu which-key use-package try switch-window s org-bullets dashboard dash counsel company beacon avy))))
+    (beacon which-key try switch-window helm swiper avy company dashboard habamax-theme exwm xelb use-ttf use-package sudo-edit powerline popup-kill-ring dmenu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
