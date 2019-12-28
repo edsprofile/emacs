e074c8746e943abee3a4e07199bfa795bb26fa40
Edwin Espinoza <espined48@gmail.com>
2019-12-27 17:36:57 -0800

    Set theme to adwaita.

--- init.el
+++ init.el
@@ -276,3 +276,18 @@
 ;;|                            Extra added variables                             |
 ;;|                                                                              |
 ;;|------------------------------------------------------------------------------|
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (adwaita)))
+ '(package-selected-packages
+   (quote
+    (which-key web-mode use-package try switch-window sudo-edit restclient magit langtool helm-swoop helm-slime helm-projectile emmet-mode dashboard beacon avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
