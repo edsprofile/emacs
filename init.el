22cef51c4152cd2575e804714128d3ea1a668637
Edwin Espinoza <espined48@gmail.com>
2019-12-27 17:43:32 -0800

    Changed spacing for testing.

--- init.el
+++ init.el
@@ -276,6 +276,7 @@
 ;;|                            Extra added variables                             |
 ;;|                                                                              |
 ;;|------------------------------------------------------------------------------|
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
